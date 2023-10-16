47ad34273ddbbab14eef095bf844df3a7f8011cb
Abdul Rahman <43729469+fingers10@users.noreply.github.com>
2023-10-16 12:15:44 +0530

    Create AppState.cs

---
+++ Code Migration/CSharp/AppState.cs
@@ -0,0 +1,21 @@
+//Class with Action Example
+internal class AppState
+{
+    public bool ShowNavigation { get; private set; } = true;
+
+    public void ToggleNavigationMenu()
+    {
+        ShowNavigation = !ShowNavigation;
+        NotifyStateChanged();
+    }
+
+    public void HideNavigationMenu()
+    {
+        ShowNavigation = false;
+        NotifyStateChanged();
+    }
+
+    internal event Action OnChange = default!;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
