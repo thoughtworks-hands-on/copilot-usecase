42367ca59307421d98ae118896bf96953446ffe8
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2023-10-19 19:57:20 +0530

    Create cdd_example.py

---
+++ CDD/cdd_example.py
@@ -0,0 +1,23 @@
+# Copilot: As an expert Python developer at a premium software consultancy, craft code that:
+# - Imports only task-specific libraries
+# - Follows Domain Driven Design (DDD)
+# - Is clear, readable, and maintainable
+# - Avoids code smells and security vulnerabilities
+# - Ensures optimal memory usage, performance, and scalability
+# - Handles errors precisely and uses utf-8 encoding
+# - Handle exceptions and pass them to the caller
+# - follows Test Driven Development (TDD), unit testable and automation test friendly
+# - follows PEP 8 standards
+# - strictly no comments / explanations
+
+
+# Generate a program for Binary Search Tree (BST) such that it has following functions:
+# 1. Insert a node
+# 2. Delete a node
+# 3. Search a node
+# 4. Find minimum node
+# 5. Find maximum node
+# 6. Find successor of a node
+
+
+# Domain class
